dc6968230447aae210961e90b25ed5e8d52a2823
ndevanthery <devantherynicolas@gmail.com>
2023-03-08 10:25:26 +0100

    register verification done

--- lib/authentification/register.dart
+++ lib/authentification/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:healthsim/Models/user_model.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+import 'package:tuple/tuple.dart';
 
 import '../database/auth.dart';
 import '../home.dart';
@@ -20,8 +22,25 @@ class _RegisterPageState extends State<RegisterPage> {
   bool passVisible = false;
   bool passConfVisible = false;
   bool isHiddenPass = true;
+  bool loading = false;
+
+  bool emailError = false;
+  bool passwordError = false;
+  bool passwordConditionError = false;
+  String errorMessage = "";
+
   @override
   Widget build(BuildContext context) {
+    errorMessage = (emailError
+        ? "your email not valid !"
+        : (passwordConditionError
+            ? '''your password must be at least contain :
+            - 8 characters
+            - one Uppercase letter
+            - one Lowercase letter
+            - one special character'''
+            : (passwordError ? "your passwords aren't matching ! " : "")));
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Register"),
@@ -55,16 +74,31 @@ class _RegisterPageState extends State<RegisterPage> {
                   const SizedBox(height: 20),
                   TextField(
                     controller: emailController,
-                    decoration: const InputDecoration(
+                    decoration: InputDecoration(
                       contentPadding: EdgeInsets.symmetric(horizontal: 10),
                       alignLabelWithHint: true,
                       label: Text("E-Mail"),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
+                          borderSide: BorderSide(
+                              color: emailError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(50))),
                       focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
+                          borderSide: BorderSide(
+                              color: emailError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(50))),
                       enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
+                          borderSide: BorderSide(
+                              color: emailError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(50))),
                     ),
                   ),
                   const SizedBox(height: 20),
@@ -88,11 +122,23 @@ class _RegisterPageState extends State<RegisterPage> {
                             ? const Icon(Icons.visibility_off)
                             : const Icon(Icons.visibility),
                       ),
-                      border: const OutlineInputBorder(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: passwordError || passwordConditionError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
                           borderRadius: BorderRadius.all(Radius.circular(50))),
-                      focusedBorder: const OutlineInputBorder(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: passwordError || passwordConditionError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
                           borderRadius: BorderRadius.all(Radius.circular(50))),
-                      enabledBorder: const OutlineInputBorder(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: passwordError || passwordConditionError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
                           borderRadius: BorderRadius.all(Radius.circular(50))),
                     ),
                   ),
@@ -117,11 +163,23 @@ class _RegisterPageState extends State<RegisterPage> {
                             ? const Icon(Icons.visibility_off)
                             : const Icon(Icons.visibility),
                       ),
-                      border: const OutlineInputBorder(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: passwordError || passwordConditionError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
                           borderRadius: BorderRadius.all(Radius.circular(50))),
-                      focusedBorder: const OutlineInputBorder(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: passwordError || passwordConditionError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
                           borderRadius: BorderRadius.all(Radius.circular(50))),
-                      enabledBorder: const OutlineInputBorder(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: passwordError || passwordConditionError
+                                  ? Color(0xFFFF00000)
+                                  : Color(0xFF000000)),
                           borderRadius: BorderRadius.all(Radius.circular(50))),
                     ),
                   ),
@@ -132,20 +190,82 @@ class _RegisterPageState extends State<RegisterPage> {
                           backgroundColor: Colors.grey[800],
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(25))),
-                      onPressed: () {
-                        register(
-                            emailController.text,
-                            passController.text,
-                            UserM(
-                                email: emailController.text,
-                                fullName: fullnameController.text));
+                      onPressed: () async {
+                        setState(() {
+                          loading = true;
+                        });
+                        Tuple3 verif = verifications(emailController.text,
+                            passController.text, passConfController.text);
+                        setState(() {
+                          emailError = !verif.item1;
+                          passwordError = !verif.item2;
+                          passwordConditionError = !verif.item3;
+                        });
+                        if (verif.item1 & verif.item2 & verif.item3) {
+                          bool result = await register(
+                              emailController.text,
+                              passController.text,
+                              UserM(
+                                  email: emailController.text,
+                                  fullName: fullnameController.text));
+                          if (!result) {
+                            var snackBar = SnackBar(
+                              content: Text(
+                                  'An error occured. Your email is already linked to an account.'),
+                              action: SnackBarAction(
+                                label: 'OK',
+                                onPressed: () {},
+                              ),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }
+                        }
+                        setState(() {
+                          loading = false;
+                        });
                       },
                       child: const Text("Create Account ")),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    errorMessage,
+                    style: TextStyle(color: Colors.red, height: 1.5),
+                  ),
+                  if (loading)
+                    SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: LoadingIndicator(
+                          indicatorType: Indicator.ballClipRotate),
+                    )
                 ]),
               ),
             ),
           ),
         ));
+  }
+
+  bool validatePassword(String password) {
+    // Define a regular expression pattern to match against the password
+    final pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*(),.?":{}|<>]).{8,}$';
+
+    // Create a RegExp object to use for matching
+    final regExp = RegExp(pattern);
+    print(regExp.hasMatch(password));
+    // Return true if the password matches the pattern, false otherwise
+    return regExp.hasMatch(password);
+  }
+
+  verifications(String email, String password, String passwordConf) {
+    final bool emailValid = RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email);
+
+    return Tuple3(
+        emailValid, password == passwordConf, validatePassword(password));
   }
 
   register(String email, String password, UserM userM) async {
@@ -158,6 +278,8 @@ class _RegisterPageState extends State<RegisterPage> {
         context,
         MaterialPageRoute(builder: (context) => HomePage()),
       );
+      return true;
     }
+    return false;
   }
 }
